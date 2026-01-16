78b97dfcdc8c2dd5e03d2067c3b0dcf9dc7c6943
Lee Fuller <leefuller@gmail.com>
2026-01-16 17:54:25 +0000

    added info to script

--- scripts/bindicator.sh
+++ scripts/bindicator.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#python3 -m venv .venv
+#source .venv/bin/activate
+#pip install selenium webdriver-manager
+
 #first cd into the directory where this script lives
 cd "$(dirname "$0")"
 
